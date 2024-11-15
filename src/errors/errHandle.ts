9a2c9fe83df1669a82156ea4b4e07be48558771a
Joaquin Olivieri <j.olivieri93@gmail.com>
2024-11-14 23:51:59 -0300

    minor changes

--- src/errors/errHandle.ts
+++ src/errors/errHandle.ts
@@ -1,8 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
 export const errorHandle = (err: any, _req: Request, res: Response) => {
-  console.log(err);
-  const status = err.status || 500;
-  const message = status === 500 ? "Internal server error" : err.message;
-  res.status(status).json({ status: "error", error: message });
+	const status = err.status || 500;
+	const message = status === 500 ? 'Internal server error' : err.message;
+	res.status(status).json({ status: 'error', error: message });
 };
